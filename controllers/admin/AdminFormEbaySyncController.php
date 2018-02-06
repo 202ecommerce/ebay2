f9f2d7fc7779e07db07b417ea8ad036edaddf019
Artem202 <apanchenko@202-ecommerce.com>
2018-02-06 18:18:12 +0100

    valiator

--- controllers/admin/AdminFormEbaySyncController.php
+++ controllers/admin/AdminFormEbaySyncController.php
@@ -19,7 +19,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2017 PrestaShop SA
+ *  @copyright 2007-2018 PrestaShop SA
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
@@ -53,7 +53,7 @@ class AdminFormEbaySyncController extends ModuleAdminController
         EbayCategoryConfiguration::deleteByIdCategory(Tools::getValue('profile'), Tools::getValue('ebay_category'));
     }
 
-    public function ajaxProcessResetBp ()
+    public function ajaxProcessResetBp()
     {
         require_once dirname(__FILE__).'/../../classes/EbayProfile.php';
         require_once dirname(__FILE__).'/../../classes/EbayRequest.php';
